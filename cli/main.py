ea9bd4b019c11d94a44f7a89b915bfe8819a3281
Royer G <royjuni3431@gmail.com>
2021-02-23 16:43:56 -0500

    add: validations and ensure to change to directories

--- cli/main.py
+++ cli/main.py
@@ -14,25 +14,46 @@ def update_and_deploy_project(project):
     repository = project.get('repository', None)
     if repository is not None:
         project_name = project['name']
-        project_path = os.path.exists(f'~/server/{project_name}/')
-        project_git = os.path.exists(f'~/server/{project_name}/.git/')
+        print('--- ' * 10)
+        print(project_name)
+
+        os.chdir(os.path.expanduser('~'))
+
+        if not os.path.isdir('server/'):
+            os.mkdir('server/')
+
+        os.chdir('server/')
+
+        project_path = os.path.isdir(f'./{project_name}/')
+        project_git = os.path.isdir(f'./{project_name}/.git/')
 
         if project_path and project_git:
-            project_branch = project['deploy-branch']
-            os.system(f'git pull origin {project_branch}')
-        else:
-            project_repository = project['repository']
-            regex = """^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$"""
-            if re.match(regex, project_repository):
-                os.system(f'git clone {project_repository}')
+            print('Update project...')
+            project_branch = project.get('deploy-branch', None)
+
+            if project_branch is not None:
+                os.chdir(project_name)
+                os.system(f'git pull origin {project_branch}')
             else:
-                print('[!] Please ensure that the repository is an url.')
-               
+                print('[!] Please ensure to provide deploy-branch.')
+
+        elif project_path:
+            print('Clone project...')
+            project_repository = project.get('repository', None)
+
+            if project_repository is not None:
+                regex = """^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$"""
+                if re.match(regex, project_repository):
+                    os.system(f'git clone {project_repository}')
+                    os.chdir(project_name)
+                else:
+                    print('[!] Please ensure that the repository is an url.')
+
         deploy_command = project.get('deploy-command', None)
-        
+
         if deploy_command is not None:
-            print('Deploy proyect...')
-            os.system(f'cd {project_path} && {deploy_command} &')
+            print('Deploy project...')
+            os.system(f'{deploy_command} &')
         else:
             print('[!] Please ensure provide the deploy_command in your projects.yml')
 
@@ -44,7 +65,8 @@ def main():
     projects = read_proyects('../projects.yaml')
     for project_name, values in projects.items():
         values['name'] = project_name
-        update_and_deploy_project(project)
+        update_and_deploy_project(values)
+
 
 if __name__ == '__main__':
     print("""
